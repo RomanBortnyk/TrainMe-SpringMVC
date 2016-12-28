b0733043f1e2fef1b16434c655be8b5c7206e19d
ninaIlyenko <zaklepka@yopmail.com>
2016-12-29 01:01:41 +0200

    add disciplineAPI test

--- src/test/java/ControllersTests.java
+++ src/test/java/ControllersTests.java
@@ -162,6 +162,38 @@ public class ControllersTests {
         verifyNoMoreInteractions(disciplineServiceMock);
 
     }
+    
+    //////////////////
+    @Test
+    public void disciplineAPIgetSortedDisciplinesToAddByUserIdTest() throws Exception{
+
+        Discipline discipline = new Discipline();
+        discipline.setId(1);
+        discipline.setName("discipline1");
+
+
+        ArrayList<Discipline>  list = new ArrayList<>();
+        list.add(discipline);
+        list.add(discipline);
+
+        when(disciplineServiceMock.getSortedDisciplinesNamesToAdd(1)).thenReturn(list);
+
+
+        mockMvc.perform(get("/api/disciplinesToAdd/{userId}", 1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].name", is("discipline1")))
+
+                .andExpect(jsonPath("$[1].id", is(1)))
+                .andExpect(jsonPath("$[1].name", is("discipline1")));
+
+
+        verify(disciplineServiceMock, times(1)).getSortedDisciplinesNamesToAdd(1);
+        verifyNoMoreInteractions(disciplineServiceMock);
+
+    }
+    ////////////
    
     //----------------------------------------------------------------
 
